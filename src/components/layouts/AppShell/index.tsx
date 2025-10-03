9fb9952c0092ee4edd3455020247618e4e2a93b1
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-10-03 11:05:30 +0700

    update new

--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -1,4 +1,3 @@
-import { Children } from "react";
 import Navbar from "../Navbar"
 
 type AppShellProps = {
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const LoginPage = () => {
+
+    const { push} = useRouter();
+    const HandlerLogin = () => {
+        push("/product");
+    };
     return (
     <div>
         <h1>Login Page</h1>
+        <button onClick={() => HandlerLogin()}>Login</button>
         <p>Belum punya akun? Registrasi
             <Link href="/auth/register"> disini</Link>
         </p>
