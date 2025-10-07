a9e80d7674cef67f3e37544ff13f7326ac668c93
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-10-07 10:41:14 +0700

    update

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const RegisterPage = () => {
+    
+    const { push } = useRouter();
+
+    const HandlerAbout = () => {
+        push("/about");
+    };
     return (
     <div>
         <h1>Register Page</h1>
+        <button onClick={() => HandlerAbout ()}>Masuk</button>
         <p>Sudah punya akun? Login
             <Link href="/auth/login"> disini</Link>
         </p>
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,4 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useState } from "react";
+
+
 const ProductPage = () => {
+    const [isLogin, setIsLogin] =  useState(false);
+    const { push } = useRouter();
+    useEffect (() => {
+        if (!isLogin) {
+            push("/auth/login");
+        }
+    }, []);
     return (
         <div>
             <h1>Product Page</h1>
